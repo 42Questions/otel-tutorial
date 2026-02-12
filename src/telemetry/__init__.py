61d5b774b3dd43ceb1f7d1fb0e5ab82cd3b97785
42Questions <46352502+42Questions@users.noreply.github.com>
2026-02-12 15:00:19 +0000

    Phase 1 complete

--- src/telemetry/__init__.py
+++ src/telemetry/__init__.py
@@ -1,4 +1,4 @@
-from opentelemetry import metrics
+from opentelemetry.metrics import set_meter_provider
 from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.metrics.export import MetricExporter, MetricReader, PeriodicExportingMetricReader
 from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
@@ -9,7 +9,7 @@ def setup_metrics():
     metric_exporter: MetricExporter = OTLPMetricExporter(endpoint=LOCAL_OTEL_EXPORTER_OTLP_METRICS_ENDPOINT)
     metric_reader: MetricReader = PeriodicExportingMetricReader(metric_exporter)
     meter_provider: MeterProvider = MeterProvider(metric_readers=[metric_reader])
-    metrics.set_meter_provider(meter_provider=meter_provider)
+    set_meter_provider(meter_provider=meter_provider)
 
 def setup_telemetry():
     setup_metrics()
